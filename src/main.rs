452d3755251219fd8e8120530afe9d4f086fac1a
AliNAbadian <nagshrizali@gmail.com>
2025-04-25 16:50:54 +0330

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// fn main() {
+//     let mut s = String::from("hello");
+
+//     s.push_str(", world!");
+//     println!("{}", s);
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+
+    let len = calc_len(&s1);
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calc_len(s: &String) -> usize {
+    s.len()
 }
