ca015f761fc3fa7e2363a1149ed1a8053ec905d5
AliNAbadian <nagshrizali@gmail.com>
2025-04-27 17:11:46 +0330

    asdasdasdasd

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,28 @@
 // }
 
 fn main() {
-    let s1 = String::from("hello");
+    let s = String::from("hello");
 
-    let len = calc_len(&s1);
-    println!("The length of '{}' is {}.", s1, len);
+    // let len = calc_len(&s1);
+    // println!("The length of '{}' is {}.", s1, len);
+
+    let word = first_word(&s);
+
+    s.clear();
+    println!("The first word is: {}", word);
 }
 
 fn calc_len(s: &String) -> usize {
     s.len()
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..] // If no space is found, return the entire string
 }
